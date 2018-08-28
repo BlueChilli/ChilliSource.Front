43f5a81102f25957fa6565e35af626023cfc6ff6
Parth Mehta <tech@iamdifferent.io>
2018-08-28 16:16:44 +1000

    :fire: react-router-redux, :arrow_up: connected-react-router, prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,53 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
-import buble from 'rollup-plugin-buble';
-import sizes from 'rollup-plugin-sizes';
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import uglify from "rollup-plugin-uglify";
+import buble from "rollup-plugin-buble";
+import sizes from "rollup-plugin-sizes";
 
-const rollupConfig =  {
-  input: './src/index.js',
+const rollupConfig = {
+  input: "./src/index.js",
   output: {
-    file: './lib/bundle.js',
-    format: 'cjs',
+    file: "./lib/bundle.js",
+    format: "cjs"
   },
   external: [
-    'react',
-    'react-redux',
-    'react-router',
-    'react-router-config',
-    'react-router-dom',
-    'react-router-redux',
-    'redux',
-    'history',
-    'lodash-es',
-    'createBrowserHistory',
-    'prop-types',
-    'object.map',
-    'createBrowserHistory'
+    "react",
+    "react-redux",
+    "react-router-config",
+    "react-router-dom",
+    "connected-react-router",
+    "redux",
+    "history",
+    "lodash-es",
+    "createBrowserHistory",
+    "prop-types",
+    "object.map",
+    "createBrowserHistory"
   ],
 
   plugins: [
     resolve([
       {
-        extensions: ['.jsx'],
-      },
+        extensions: [".jsx"]
+      }
     ]),
     babel({
-      exclude: '**/node_modules/**',
+      exclude: "**/node_modules/**"
     }),
     commonjs({
-      include: [
-        'node_modules/**',
-      ],
-      exclude: [
-        'node_modules/process-es6/**',
-      ],
+      include: ["node_modules/**"],
+      exclude: ["node_modules/process-es6/**"],
       globals: {
-        react: 'React',
-      },
-
+        react: "React"
+      }
     }),
     buble(),
-    sizes(),
-  ],
-
+    sizes()
+  ]
 };
 
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   rollupConfig.plugins.push(
     uglify({
       compress: {
@@ -65,7 +57,7 @@ if (process.env.NODE_ENV === 'production') {
         warnings: false
       }
     })
-  )
-};
+  );
+}
 
 export default rollupConfig;
